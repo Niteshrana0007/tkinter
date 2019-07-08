1670cf7526a45ca94df8a90f3120edae4f7cee4a
Niteshrana0007 <52644947+Niteshrana0007@users.noreply.github.com>
2019-07-07 21:56:34 -0700

    Add files via upload

---
+++ GUIcal.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+frame1 = Frame(window,width=200,height=300)
+
+frame1.pack()
+
+
+label1 = Label(frame1,text= "enter_first_number")
+label1.pack()
+
+label2 = Label(frame1,text= "enter_second_number")
+label2.pack()
