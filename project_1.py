82683322cae082d3f8e1f3891b6f04a9d2763f8c
Niteshrana0007 <52644947+Niteshrana0007@users.noreply.github.com>
2019-07-12 10:25:57 +0530

    Add files via upload

---
+++ project_1.py
@@ -0,0 +1,21 @@
+import json
+class DataHandler:
+    def __init__(self,file_name):
+        data_info = open(file_name,'r')
+        self.data = json.load(data_info)
+        #print(self.data)
+
+    def get_categories(self):
+        list_data = []
+        for element in self.data["category_list"]:
+            list_data.append(element['name'])
+        return list_data
+
+    def  get_data_for_category(self,name):
+        list_data = []
+        for element in self.data["category_list"]:
+            if element["name"].lower() == name.lower():
+                list_data = element["list"]
+        return list_data
+        
+            
---
+++ project_2.py
@@ -0,0 +1,63 @@
+import tkinter
+from functools import partial
+from project_1 import DataHandler
+
+
+def get_data_for_category(name):
+     return data_handler.get_data_for_category(name)
+
+def open_category(name):
+    
+     # delete old labels
+     for element in category_data_labels:
+         element.configure(text="")
+         del element
+
+     list_data_for_category = get_data_for_category(name)
+     for i in range(len(list_data_for_category)):
+         element = list_data_for_category[i]
+         label = tkinter.Label(window,text = element)
+         label.grid(column = 0,row  = i+1)
+         category_data_labels.append(label)
+
+window = tkinter.Tk()
+window.title("Children Book")
+window.geometry('300x500')
+data_handler = DataHandler("project.json")
+category_lists = data_handler.get_categories()
+print(category_lists)
+category_data_labels = []
+
+'''canvas = tkinter.Canvas(window,bg='red')
+frame = tkinter.Frame(canvas,bg='green')
+vsb = tkinter.Scrollbar(canvas,command=canvas.yview)
+canvas.configure(yscrollcommand=vsb.set)
+
+
+vsb.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+canvas.pack(side=tkinter.BOTTOM,fill=tkinter.BOTH,expand=True)
+canvas.create_window(window=frame,anchor=tkinter.NW)'''
+
+buttons_container = tkinter.Frame(window)
+buttons_container.pack(side=tkinter.TOP, fill = tkinter.X)
+
+for i in range(len(category_lists)):
+    element = category_lists[i]
+    command_func = partial(open_category,element)
+    button  = tkinter.Button(buttons_container,text = element,command = command_func)
+    button.grid(row=0,column=i)
+
+container = tkinter.Frame(window,bg='green')
+container.pack(side=tkinter.TOP,expand=True,fill = tkinter.BOTH)
+
+scrollbar1 = tkinter.Scrollbar(container)
+scrollbar1.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+
+data_canvas = tkinter.Canvas(container,bg='red')
+data_canvas.pack(side=tkinter.TOP)
+
+canvas_frame = tkinter.Frame(data_canvas)
+canvas_frame.pack(side = tkinter.TOP,expand=True,fill=tkinter.BOTH)
+
+
+window.mainloop()
